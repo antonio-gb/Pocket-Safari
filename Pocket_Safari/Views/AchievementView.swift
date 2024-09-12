afd0c486c4430ecf623c8403161296b72b167697
Fernando Meneses <fernando.perezms@udlap.mx>
2024-09-12 15:32:21 -0600

    first commit

--- Pocket_Safari/Views/AchievementView.swift
+++ Pocket_Safari/Views/AchievementView.swift
@@ -13,8 +13,28 @@ struct AchievementView: View {
     var body: some View {
         ZStack {
             Rectangle()
-                .fill(Color.white)
-            Text("AchievmentView")
+                .fill(Color.black)
+                .edgesIgnoringSafeArea(.all) // This makes the black background cover the full screen
+                
+            VStack {
+                // Green rectangle at the top
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(height: 50) // Adjust the height as needed
+                    .cornerRadius(10)
+                    .overlay(
+                        Text("Lorem Ipsum")
+                            .foregroundColor(.white)
+                    )
+                    .padding(.top, 20) // Adjust top margin
+                    .padding(.horizontal, 20) // Adds horizontal margins
+
+                Spacer() // Pushes the content down
+            }
         }
     }
+}
+
+#Preview {
+    AchievementView()
 }
