274246445a86ddb9050e5e15c270618f92013db6
HugeErick <erick.parada101@gmail.com>
2024-10-03 10:59:55 -0600

    chore: worked on encyclopedia view, added skeletons and rename the txt helper file

--- Pocket_Safari/Helpers/ColorPalette.swift
+++ Pocket_Safari/Helpers/ColorPalette.swift
@@ -15,4 +15,5 @@ struct ColorPalette {
     let pocketShadeGreen = Color.init(red: 140/255, green: 199/255, blue: 63/255)
     let pocketDarkGreen = Color.init(red: 36/255, green: 125/255, blue: 69/255)
     let pocketBlack = Color.init(red: 9/255, green: 9/255, blue: 11/255)
+    let pocketGray = Color.gray.opacity(0.6)
 }
--- Pocket_Safari/Views/Encyclopedia.swift
+++ Pocket_Safari/Views/Encyclopedia.swift
@@ -1,9 +1,3 @@
-//
-//  Encyclopedia.swift
-//  Pocket_Safari
-//
-//  Created by iOS Lab on 27/08/24.
-//
 import SwiftUI
 
 struct EncyclopediaView: View {
@@ -11,9 +5,59 @@ struct EncyclopediaView: View {
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(Color.white)
-            Text("EncyclopediaView")
+            colorP.pocketBlack.edgesIgnoringSafeArea(.all) // Background color across the entire view
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    // encyclopdia based on the fauna and flora discovered
+                    SkeletonRectangle(colorP: colorP)
+                    SkeletonRectangle(colorP: colorP)
+                }
+                .padding()
+            }
         }
     }
+}
+
+struct SkeletonRectangle: View {
+    let colorP: ColorPalette
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(colorP.pocketDarkGreen)
+                .frame(height: 500) // Height for the entire rectangle
+            
+            VStack(alignment: .leading, spacing: 20) {
+                // Image placeholder at the top
+                Rectangle()
+                    .fill(colorP.pocketGray) // Image placeholder color
+                    .frame(height: 80)
+                    .cornerRadius(10)
+                    .padding(.top, -110) // Reduce top margin of the image placeholder
+
+                // Skeleton text placeholders (need to make a loop here uwu)
+                VStack() {
+                    Rectangle()
+                        .fill(colorP.pocketGray) // Text placeholder
+                        .frame(height: 20) // Placeholder for text line 1
+
+                    Rectangle()
+                        .fill(colorP.pocketGray)
+                        .frame(height: 20) // Placeholder for text line 2
+
+                    Rectangle()
+                        .fill(colorP.pocketGray)
+                        .frame(height: 20) // Placeholder for text line 3
+                }
+                .padding(.top, 20) // Reduced spacing between image and text
+            }
+            .padding() // Padding inside the rectangle
+        }
+        .padding() // Padding around the rectangle
+    }
+}
+
+#Preview {
+    EncyclopediaView() 
 }
